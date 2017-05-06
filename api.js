15ab764ab1e3bd9529571adcd24168db154324a3
goumang2010 <goumang2010@live.com>
2017-05-06 20:50:12 +0800

    comment for test

--- api.js
+++ api.js
@@ -7,7 +7,10 @@ export default {
             }
             var data;
             if (res && (data = res.data) && (data = data.result) && data.length) {
-                // data = data.filter(x => x.pointName.includes('-a0'));
+                // data = data.filter(x => /a(\d|[0-2]\d)$/.test(x.pointName));
+                // data = data.filter(x => /a83$/.test(x.pointName));
+				// data = data.filter(x => /(a14[67]|a49|a50|a51|a52)$/.test(x.pointName));		
+                // data = data.filter(x => /(a26)$/.test(x.pointName));		
                 return data;
             } else {
                 return Promise.reject('No heat info!');
